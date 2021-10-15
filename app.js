62268bc6c1b1cefecbcc660b0b099f66f7553154
s-moe <sarahannmoe@gmail.com>
2021-10-15 12:22:51 -0700

    toggle classes

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const app = Vue.createApp({
    data() {
        return {
+           url: 'http://www.google.com',
            showBooks: true,
-           title: "The Final Empire",
-           author: "Brandon Sanderson", 
-           age: 45,
-           x: 0,
-           y: 0
+           books: [
+               {
+                   title: 'name of the wind',
+                   author: 'patrick rothfuss',
+                   img: 'assests/SarahMoeAboutImg.jpg',
+                   isFav: true
+               },
+               {
+                   title: 'the way of kings',
+                   author: 'brandon sanderson',
+                   img: 'assests/SarahMoeAboutImg.jpg',
+                   isFav: false
+               },
+               {
+                   title: 'the final empire',
+                   author: 'brandon sanderson',
+                   img: 'assests/SarahMoeAboutImg.jpg',
+                   isFav: true
+               }
+           ]
        }
    },
    methods: {
@@ -22,8 +38,15 @@ const app = Vue.createApp({
        handleMousemove(e) {
             this.x = e.offsetX
             this.y = e.offsetY
+       },
+       toggleFav(book) {
+           book.isFav = !book.isFav
        }
    }
 })
 
 app.mount('#app')
+
+// Challenge - Add to Favs 
+// - attach a click event to each li tag (for each book)
+// - when a user clicks an li, toggle 'isFav' property of that book
