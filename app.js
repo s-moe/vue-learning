16e60d2f21a8cd82d95370b93f8abe82f8086f5f
s-moe <sarahannmoe@gmail.com>
2021-10-15 10:45:30 -0700

    conditional rendering

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const app = Vue.createApp({
    data() {
        return {
+           showBooks: true,
            title: "The Final Empire",
            author: "Brandon Sanderson", 
            age: 45
        }
    },
    methods: {
-       changeTitle(title) {
-        // this.title = "Words of Radiance"
-        this.title = title
+       toggleShowBooks(){
+           this.showBooks = !this.showBooks
        }
    }
 })
