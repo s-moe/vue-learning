e4bdc27e3a93d67600693e47fc1073652a522227
s-moe <sarahannmoe@gmail.com>
2021-10-15 10:32:11 -0700

    methods

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const app = Vue.createApp({
-   
+   data() {
+       return {
+           title: "The Final Empire",
+           author: "Brandon Sanderson", 
+           age: 45
+       }
+   },
+   methods: {
+       changeTitle(title) {
+        // this.title = "Words of Radiance"
+        this.title = title
+       }
+   }
 })
 
 app.mount('#app')
